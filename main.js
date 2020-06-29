36d2352d8cd55668433581e48ed94dbfdd0bbbaa
n1cb0tt <65509016+n1cb0tt@users.noreply.github.com>
2020-06-29 10:25:20 +0200

    main.js + json files

---
+++ main.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Pablo'));
+console.log(chalk.rgb(120, 40, 256).bold.bgWhite('Shanti'));
+console.log(chalk.hex('#009900')('Marine'));
+console.log(chalk.yellow.underline('Trad'));
+
+
